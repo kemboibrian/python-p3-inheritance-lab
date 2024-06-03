6a0e597f41ffd0fb15025e5bb9abea82453bf2a7
Brian Kemboi <brnkemboi@gmail.com>
2024-06-03 22:06:12 +0300

    complete

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,11 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+     def __init__(self, first_name, last_name, knowledge = []):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.knowledge = knowledge
+
+     def learn(self, string):
+        self.string = string
+        return self.knowledge.append(self.string)
